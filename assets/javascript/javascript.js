923b5b804c1d2aa121a764693cc5e6dfe75ee3cf
Romulo Albuquerque <rjaalbuquerque@gmail.com>
2019-04-28 16:48:59 +0000

    Added content to large screen menu

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,10 @@
+// Scroll top button
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
+// Intechanging pages
 function openAbout() {
   document.getElementById("sidenav-about").style.width = "45vw";
   document.getElementById("sidenav-resume").style.width = "0";
