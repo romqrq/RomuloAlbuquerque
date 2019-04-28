4f2a379358ba70ce72d2c120543d2e83c59bc595
Romulo Albuquerque <rjaalbuquerque@gmail.com>
2019-04-28 19:45:12 +0000

    About card problem

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,28 +6,28 @@ function topFunction() {
 
 // Intechanging pages
 function openAbout() {
-  document.getElementById("sidenav-about").style.width = "45vw";
+  document.getElementById("sidenav-about").style.width = "47vw";
   document.getElementById("sidenav-resume").style.width = "0";
   document.getElementById("sidenav-portfolio").style.width = "0";
   document.getElementById("sidenav-contact").style.width = "0";
 }
 function openResume() {
   document.getElementById("sidenav-about").style.width = "0";
-  document.getElementById("sidenav-resume").style.width = "45vw";
+  document.getElementById("sidenav-resume").style.width = "47vw";
   document.getElementById("sidenav-portfolio").style.width = "0";
   document.getElementById("sidenav-contact").style.width = "0";
 }
 function openPortfolio() {
   document.getElementById("sidenav-about").style.width = "0";
   document.getElementById("sidenav-resume").style.width = "0";
-  document.getElementById("sidenav-portfolio").style.width = "45vw";
+  document.getElementById("sidenav-portfolio").style.width = "47vw";
   document.getElementById("sidenav-contact").style.width = "0";
 }
 function openContact() {
   document.getElementById("sidenav-about").style.width = "0";
   document.getElementById("sidenav-resume").style.width = "0";
   document.getElementById("sidenav-portfolio").style.width = "0";
-  document.getElementById("sidenav-contact").style.width = "45vw";
+  document.getElementById("sidenav-contact").style.width = "47vw";
 }
 
 
